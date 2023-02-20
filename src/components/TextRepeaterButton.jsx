ec97203f1ce28022b5bc95fe29c4ab840bffa589
javascriptsucks <evix1988@live.cn>
2023-02-20 12:04:44 -0600

    map-repeat-text

--- src/components/TextRepeaterButton.jsx
+++ src/components/TextRepeaterButton.jsx
@@ -3,19 +3,20 @@ import React, { useState } from "react";
 function TextRepeaterButton() {
 	const [repeat, setRepeat] = useState(1);
 	const textArray = [];
+	for (let i = 0; i < repeat; i++) {
+		textArray.push(<span key={i}>I like this text</span>);
+	}
+	const clickHandler = () => {
+		setRepeat(repeat + 1);
+		console.log(textArray);
+	};
 	return (
 		<button
-			onClick={() => {
-				setRepeat(repeat + 1);
-				for (let i = 0; i < repeat; i++) {
-					textArray.push(<span key={i}>I like this text</span>);
-				}
-				console.log(textArray);
-			}}
+			onClick={clickHandler}
 			className='TextRepeaterButton'>
-			{/* {textArray.map((ele) =>
-				ele;
-			)} */}
+			{textArray.map((ele) => {
+				return { ...ele };
+			})}
 			<span>I like this text</span>
 		</button>
 	);
